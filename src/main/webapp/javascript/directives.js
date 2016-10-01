619c7ffe7fb83b1e98cde6b2559bccfa43c29156
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-10-01 18:10:20 +0200

    Add directives tags

--- src/main/webapp/javascript/directives.js
+++ src/main/webapp/javascript/directives.js
@@ -1,3 +1,20 @@
-/**
- * Created by ruben on 1/10/16.
- */
+angular.module('trainingTrackerApp')
+
+    // include the 'navbar.html' into the <navbar> tag
+    .directive('navbar', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/components/navbar.html',
+            controller: 'navbarController'
+        }
+    })
+
+    //include the 'exerciserCard.html' into the <exerciserCard> tag
+    .directive('exerciserCard', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/components/exerciserCard.html',
+            controller: 'exerciserCardController',
+            scope: true
+        }
+    });
