765d29910aaf7f9943299ac8d1568d79fc6688ed
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-10-26 21:20:46 +0200

    Add first iteration controller of the home page

--- src/main/webapp/javascript/controllers/homeController.js
+++ src/main/webapp/javascript/controllers/homeController.js
@@ -1,3 +1,44 @@
-/**
- * Created by ruben on 1/10/16.
- */
+angular.module('trainingTrackerApp')
+
+    .controller('exerciseCardController', ['$scope', '$state', 'exerciseService',
+        function ($scope, $state, exerciseService) {
+
+            $scope.exerciseList = []; //exercise list
+            // exercise list server request
+            $scope.getExerciseList = function () {
+                exerciseService.getExerciseList(function (exercises) {
+                    $scope.exerciseList = exercises;
+                });
+            };
+            $scope.getExerciseList();
+
+            // feedback handling variables
+            $scope.error = false;
+            $scope.success = false;
+            $scope.successMsg = "";
+            $scope.errorMsg = "";
+
+            // hide the error mensage
+            $scope.hideError = function () {
+                $scope.errorMsg = "";
+                $scope.error = false;
+            };
+            // show the error mensage
+            var showError = function (error) {
+                $scope.errorMsg = error;
+                $scope.error = true;
+            };
+
+            // show the success mensage
+            var showSuccess = function (message) {
+                $scope.successMsg = message;
+                $scope.success = true;
+            };
+
+            // hide the success mensage
+            $scope.hideSuccess = function () {
+                $scope.success = false;
+                $scope.successMsg = "";
+            };
+
+        }]);
