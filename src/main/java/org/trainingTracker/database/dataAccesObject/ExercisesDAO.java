381a762c0cdc21916a18af02b1481ebe9152a6ec
Unknown <sergiozz95@gmail.com>
2016-10-31 19:23:52 +0100

    Modify Exercise's parametres names in order to match with the API

--- src/main/java/org/trainingTracker/database/dataAccesObject/ExercisesDAO.java
+++ src/main/java/org/trainingTracker/database/dataAccesObject/ExercisesDAO.java
@@ -3,7 +3,6 @@ package org.trainingTracker.database.dataAccesObject;
 import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 import org.trainingTracker.database.conection.ConnectionPool;
 import org.trainingTracker.database.valueObject.ExerciseVO;
-import org.trainingTracker.database.valueObject.UserVO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -12,19 +11,22 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.trainingTracker.database.dataAccesObject.UsersDAO.DBF_NICK;
-
 /**
  * Exercises's Data Access Object
  */
 public class ExercisesDAO {
 
     //Database Field
-    public static final String DBF_TABLE_NAME = "Exercises";
-    public static final String DBF_ID = "_id";
-    public static final String DBF_NAME = "name";
-    public static final String DBF_MUSCLEGROUP = "muscle_group";
-    public static final String DBF_PREDEFINED = "predefined";
+    public static final String DBF_EXERCISES_TABLE_NAME = "Exercises";
+    public static final String DBF_EXERCISE_ID = "_id";
+    public static final String DBF_EXERCISE_NAME = "name";
+    public static final String DBF_EXERCISE_MUSCLEGROUP = "muscle_group";
+    public static final String DBF_EXERCISE_PREDEFINED = "predefined";
+    public static final String DBF_OWN_TABLE_NAME = "Own";
+    public static final String DBF_OWN_NICK = "nick";
+    public static final String DBF_OWN_EXERCISE = "exercise";
+
+
 
     /**
      * Adds the given exercise to the data base.
@@ -40,7 +42,7 @@ public class ExercisesDAO {
 
             PreparedStatement stmt = conn.prepareStatement(
                 String.format( "INSERT INTO %s ( %s, %s ) VALUES (?, ?);",
-                    DBF_TABLE_NAME, DBF_NAME, DBF_MUSCLEGROUP));
+                    DBF_EXERCISES_TABLE_NAME, DBF_EXERCISE_NAME, DBF_EXERCISE_MUSCLEGROUP));
             stmt.setString(1,name);
             stmt.setString(2,muscleGroup);
 
@@ -64,15 +66,20 @@ public class ExercisesDAO {
      * Returns a list with all exercises
      * @return
      */
-	public static List<ExerciseVO> listAllExercises(){
+	public static List<ExerciseVO> listAllExercises(String user){
 		Connection conn = null;
 		try{
 			Class.forName(ConnectionPool.JDBC_DRIVER);
 			conn = ConnectionPool.requestConnection();
 
             PreparedStatement stmt = conn.prepareStatement(
-                String.format( "SELECT  %s, %s, %s, %s FROM %s;",
-                    DBF_ID, DBF_NAME, DBF_MUSCLEGROUP, DBF_PREDEFINED, DBF_TABLE_NAME));
+                String.format( "SELECT ex.%2$s, ex.%3$s, ex.%4$s, ex.%5$s " +
+                        "FROM %1$s ex, %6$s own WHERE ex.%2$s=own.%7$s AND own.%8$s=?;",
+                    DBF_EXERCISES_TABLE_NAME, DBF_EXERCISE_ID, DBF_EXERCISE_NAME,
+                    DBF_EXERCISE_MUSCLEGROUP, DBF_EXERCISE_PREDEFINED,
+                    DBF_OWN_TABLE_NAME, DBF_OWN_EXERCISE, DBF_OWN_NICK));
+
+            stmt.setString(1,user);
 
             ResultSet rs = stmt.executeQuery();
 
@@ -81,8 +88,8 @@ public class ExercisesDAO {
             rs.first();
 
             do{
-				list.add( new ExerciseVO(rs.getInt(DBF_ID), rs.getString(DBF_NAME), rs.getString(DBF_MUSCLEGROUP),
-                    rs.getBoolean(DBF_PREDEFINED)) );
+				list.add( new ExerciseVO(rs.getInt(DBF_EXERCISE_ID), rs.getString(DBF_EXERCISE_NAME), rs.getString(DBF_EXERCISE_MUSCLEGROUP),
+                    rs.getBoolean(DBF_EXERCISE_PREDEFINED)) );
 			} while( rs.next() );
 
 			return list;
