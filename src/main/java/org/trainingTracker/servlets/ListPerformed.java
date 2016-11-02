f823a5db1b82b17c442bddc2118bbdc5f7fbf3af
Adrián Martínez <576307@celes.unizar.es>
2016-11-02 10:47:37 +0100

    listPerformed backend. ExerciseVO serialize changed

---
+++ src/main/java/org/trainingTracker/servlets/ListPerformed.java
@@ -0,0 +1,64 @@
+package org.trainingTracker.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONException;
+
+import org.trainingTracker.database.dataAccesObject.ExercisesDAO;
+import org.trainingTracker.database.valueObject.ExerciseVO;
+
+/**
+ * Servlet implementation class ListPerformed
+ */
+@WebServlet("/listPerformed")
+public class ListPerformed extends HttpServlet {
+    
+    private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListPerformed() {
+        super();
+    }
+
+    @Override
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Reads User header and substracts user
+        String name = request.getHeader("user");
+        
+        try {
+            // Search for predifined exercises in BD
+            JSONArray jsonExercises = new JSONArray();
+            JSONObject exercise;
+            
+            response.setStatus(HttpServletResponse.SC_OK);
+            for (ExerciseVO vo : ExercisesDAO.listAllExercises()) {
+                exercise = JSONObject.fromObject(vo.serialize());
+                exercise.remove("id");
+                exercise.remove("predefines");
+                jsonExercises.add(exercise);
+            }
+            response.setContentType("application/json; charset=UTF-8");
+            response.getWriter().write(jsonExercises.toString());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println("Error interno en el servidor. Vuelva intentarlo más tarde");
+        }
+    }
+    
+}
