bc4f6212a8512f08d5a55fa55ebc4182146e8bde
Unknown <sergiozz95@gmail.com>
2016-11-02 23:36:30 +0100

    Updates DeployDB sript

--- deployDB.sql
+++ deployDB.sql
@@ -46,3 +46,11 @@ CREATE TABLE Records (
     foreign key(user_nick) references Users(nick),
     primary key(exercise, user_nick, record_date)
 );
+USE trainingTracker;
+INSERT INTO MuscleGroup (name) VALUES ('Pecho');
+INSERT INTO MuscleGroup (name) VALUES ('Espalda');
+INSERT INTO MuscleGroup (name) VALUES ('Brazo');
+INSERT INTO MuscleGroup (name) VALUES ('Pierna');
+
+INSERT INTO Exercises (name, muscle_group) VALUES ('Dominadas', 'Espalda');
+INSERT INTO Exercises (name, muscle_group) VALUES ('Press Banca', 'Pecho');
