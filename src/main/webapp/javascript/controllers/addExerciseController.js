4fb48f9da4ced17ebaa8747ba75353107183376b
Ruben Moreno Jimeno <nebur395@hotmail.com>
2016-11-06 13:14:58 +0100

    Add first iteration of addExercise page

---
+++ src/main/webapp/javascript/controllers/addExerciseController.js
@@ -0,0 +1,34 @@
+angular.module('trainingTrackerApp')
+
+    .controller('addExerciseCtrl', ['$scope', '$state', function ($scope, $state) {
+
+
+        // feedback handling variables
+        $scope.error = false;
+        $scope.success = false;
+        $scope.successMsg = "";
+        $scope.errorMsg = "";
+
+        // hide the error mensage
+        $scope.hideError = function () {
+            $scope.errorMsg = "";
+            $scope.error = false;
+        };
+        // show the error mensage
+        var showError = function (error) {
+            $scope.errorMsg = error;
+            $scope.error = true;
+        };
+
+        // show the success mensage
+        var showSuccess = function (message) {
+            $scope.successMsg = message;
+            $scope.success = true;
+        };
+
+        // hide the success mensage
+        $scope.hideSuccess = function () {
+            $scope.success = false;
+            $scope.successMsg = "";
+        };
+    }]);
