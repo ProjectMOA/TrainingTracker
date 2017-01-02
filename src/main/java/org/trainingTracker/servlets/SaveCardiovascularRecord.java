78fa196c751078781738448dcab4b21d2919ce55
Adrián Martínez <a.martinez1889@gmail.com>
2017-01-02 13:56:18 +0100

    Added SaveCardiovascularRecord servlet. web.xml updated.

---
+++ src/main/java/org/trainingTracker/servlets/SaveCardiovascularRecord.java
@@ -0,0 +1,206 @@
+package org.trainingTracker.servlets;
+
+import net.sf.json.JSONObject;
+import org.trainingTracker.database.dataAccesObject.CardioExercisesDAO;
+import org.trainingTracker.database.valueObject.CardioExerciseVO;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Iterator;
+
+/**
+ * Servlet implementation class SaveCardiovascularRecord
+ */
+@WebServlet("/saveCardiovascularRecord")
+public class SaveCardiovascularRecord extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SaveCardiovascularRecord() {
+        super();
+    }
+
+    @Override
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        boolean error = false;
+        String user = "";
+        String exercise = "";
+        String distance = "";
+        String time = "";
+        String intensity = "";
+        
+        // Reads a JSON Object from request and captures his fields
+        JSONObject json = null;
+        try {
+            json = ServletCommon.readJSON(request.getReader());
+            user = json.getString("user");
+            exercise = json.getString("id");
+            distance = json.getString("distance").replace(",", ".");
+            time = json.getString("time");
+            intensity = json.getString("intensity").replace(",", ".");
+        }
+        catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().println("Error interno en el servidor. Vuelva intentarlo más tarde");
+            error = true;
+        }
+        
+        // Field revision
+        if (!isValidDistance(distance, response) |
+            !isValidTime(time, response) |
+            !isValidintensity(intensity, response)) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            error = true;
+        }
+        
+        if (!error) {
+            try {
+                // Creates an record in BD
+                if (CardioRecordsDAO.addRecord(Integer.parseInt(exercise), user, Double.parseDouble(distance),
+                                         Integer.parseInt(time), Double.parseDouble(intensity))) {
+                    // Search for performed exercises in BD
+                    JSONArray jsonExercises = new JSONArray();
+                    JSONArray jsonCardioExercises = new JSONArray();
+                    JSONObject jExercise, jRecord;
+                    List<RecordVO> list;
+                    
+                    response.setStatus(HttpServletResponse.SC_OK);
+                    for (ExerciseVO vo : ExercisesDAO.listUserExercises(user)) {
+                        jExercise = JSONObject.fromObject(vo.serialize());
+                        if(!(list=RecordsDAO.listRecords(user, vo.getId(), 1, 1)).isEmpty()){
+                            jRecord = JSONObject.fromObject(list.get(0).serialize());
+                            jRecord.remove("exercise");
+                            jRecord.remove("nick");
+                            jRecord.remove("commentary");
+                            jRecord.remove("date");
+                            jExercise.putAll(jRecord);
+                        }
+                        jsonExercises.add(jExercise);
+                    }
+
+                    List<CardioRecordVO> list;
+                    for (CardioExerciseVO vo : CardioExercisesDAO.listUserExercises(user)) {
+                        jExercise = JSONObject.fromObject(vo.serialize());
+                        if(!(list=CardioRecordsDAO.listRecords(user, vo.getId(), 1, 1)).isEmpty()){
+                            jRecord = JSONObject.fromObject(list.get(0).serialize());
+                            jExercise.putAll(jRecord);
+                        }
+                        jsonCardioExercises.add(jExercise);
+                    }
+                    
+                    response.setContentType("application/json; charset=UTF-8");
+                    response.getWriter().write(jsonExercises.toString());
+                    response.getWriter().write(jsonCardioExercises.toString());
+                }
+                else {
+                    response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                    response.getWriter().println("Error interno en el servidor. Vuelva intentarlo más tarde");
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                response.getWriter().println("Error interno en el servidor. Vuelva intentarlo más tarde");
+            }
+        }
+    }
+    
+    /**
+     * @param str
+     * @param response
+     * @returns true if str confirms distance specifications
+     */
+    static boolean isValidDistance (String str, HttpServletResponse response) throws IOException {
+        boolean error = false;
+        
+        try {
+            if (!(Double.parseDouble(str) > 0)) {
+                error = true;
+            }
+        }
+        catch (NullPointerException e) {
+            error = true;
+        }
+        catch (NumberFormatException e) {
+            error = true;
+        }
+        
+        if (error) {
+            response.getWriter().println("Distancia no válida");
+        }
+        
+        return !error;
+    }
+    
+    /**
+     * @param str
+     * @param response
+     * @returns true if str confirms time specifications
+     */
+    static boolean isValidTime (String str, HttpServletResponse response) throws IOException {
+        boolean error = false;
+        
+        try {
+            if (!(Integer.parseInt(str) > 0)) {
+                error = true;
+            }
+        }
+        catch (NullPointerException e) {
+            error = true;
+        }
+        catch (NumberFormatException e) {
+            error = true;
+        }
+        
+        if (error) {
+            response.getWriter().println("Tiempo no válido");
+        }
+        
+        return !error;
+    }
+    
+    /**
+     * @param str
+     * @param response
+     * @returns true if str confirms intensity specifications
+     */
+    static boolean isValidIntensity (String str, HttpServletResponse response) throws IOException {
+        boolean error = false;
+        
+        try {
+            if (!(Double.parseDouble(str) > 0)) {
+                error = true;
+            }
+        }
+        catch (NullPointerException e) {
+            error = true;
+        }
+        catch (NumberFormatException e) {
+            error = true;
+        }
+        
+        if (error) {
+            response.getWriter().println("Intensidad no válida");
+        }
+        
+        return !error;
+    }
+    
+}
